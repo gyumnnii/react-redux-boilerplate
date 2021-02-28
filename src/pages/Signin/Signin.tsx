c1fde50c3742a39bad8ffdc6c68c5d09099d166d
gyumnnii <gyumin.choi@navercorp.com>
2021-03-01 00:28:32 +0900

    Add pages prototype with router

---
+++ src/pages/Signin/Signin.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Signin=()=>{
+  return(
+    <div>Hello Signin Page!</div>
+  )
+}
+export default Signin
---
+++ src/router/RouterContainer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Home from '../pages/Home/Home'
+import Signin from '../pages/Signin/Signin'
+import Signup from '../pages/Signup/Signup'
+
+const RouterContainer=()=>{
+  return(
+    <BrowserRouter>
+      <Route exact path="/"  component={Home}/>
+      <Route path="/signin"  component={Signin}/>
+      <Route path="/signup"  component={Signup}/>
+    </BrowserRouter>
+  )
+}
+export default RouterContainer
